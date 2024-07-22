9c784809497070c2153b6c1f15177e17732b0fe6
Retraxi <raphael_tanai@dlsu.edu.ph>
2024-07-22 23:41:45 +0800

    changed page name to reflect properly

--- app/src/main/java/com/mobdeve/s11/group5/shopfreemobileapp/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s11/group5/shopfreemobileapp/MainActivity.kt
@@ -5,12 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Recycler
-import com.mobdeve.s11.group5.shopfreemobileapp.databinding.HomePageBinding
 import com.mobdeve.s11.group5.shopfreemobileapp.databinding.SignupBinding
+import com.mobdeve.s11.group5.shopfreemobileapp.databinding.TitlePageBinding
 
 class MainActivity : ComponentActivity() {
     //Requisites
-    private lateinit var binding: HomePageBinding
+    private lateinit var binding: TitlePageBinding
     private lateinit var recyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
