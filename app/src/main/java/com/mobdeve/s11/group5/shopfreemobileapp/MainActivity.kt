7001c65552bb71683766d347fcb3ed870b18d0f5
Retraxi <raphael_tanai@dlsu.edu.ph>
2024-07-22 22:14:09 +0800

    profile page and cart added

--- app/src/main/java/com/mobdeve/s11/group5/shopfreemobileapp/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s11/group5/shopfreemobileapp/MainActivity.kt
@@ -3,8 +3,15 @@ package com.mobdeve.s11.group5.shopfreemobileapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Recycler
+import com.mobdeve.s11.group5.shopfreemobileapp.databinding.HomePageBinding
+import com.mobdeve.s11.group5.shopfreemobileapp.databinding.SignupBinding
 
 class MainActivity : ComponentActivity() {
+    //Requisites
+    private lateinit var binding: HomePageBinding
+    private lateinit var recyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
